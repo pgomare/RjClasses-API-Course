9ca851aef1ed894f15ccd08bae333d249092b874
pgomare <pundlikgomare1015@gmail.com>
2022-07-24 15:30:50 +0530

    FIrst Commit

---
+++ rjclasses-course-API/src/main/java/com/rjclasses/course/RjclassesCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.rjclasses.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjclassesCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RjclassesCourseApiApplication.class, args);
+	}
+
+}
---
+++ rjclasses-course-API/src/test/java/com/rjclasses/course/RjclassesCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjclasses.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RjclassesCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
